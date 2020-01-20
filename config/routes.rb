240bf1ca9ba8ec94cf32f59c8dab92283af97615
JackMarx <seriousfools@gmail.com>
2020-01-20 12:05:00 -0600

    partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/first_contact' => 'contacts#first_contact'
-    get '/all_contacts' => 'contacts#all_contacts'
+    get '/contacts' => 'contacts#index'
+    post '/contacts' => 'contacts#create'
+    get '/contacts/:id' => 'contacts#show'
+    patch '/contacts/:id' => 'contacts#update'
+    delete '/contacts/:id' => 'contacts#destroy'
   end
 end

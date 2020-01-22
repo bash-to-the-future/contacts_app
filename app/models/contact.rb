5cb60f8cb2a247053d05beb6f9288a73c0c58330
JackMarx <seriousfools@gmail.com>
2020-01-22 11:58:36 -0600

    happy / sad path

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A(\w|[.])+[@]\w+[.][a-z]{2,}\z/i, message: "is wrong"
+
   def full_name
     "#{first_name} #{last_name}"
   end
